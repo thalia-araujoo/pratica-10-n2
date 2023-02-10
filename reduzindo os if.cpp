fdffd6172d93c2391242b1e06c1cf9d8952fd16e
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-02-10 16:51:21 -0300

    Add files via upload

---
+++ reduzindo os if.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+//o ponteiro tem que ser na assinatura, e para x e y ser transfordo em poteiro coloca-se o *//
+// quando faço ponteiros eu passo os endereços.
+
+void troca(int *x, int *y){
+	int aux = *x;
+	*x = *y;
+	*y = aux;
+
+}
+
+
+int main(){
+	int a, b, c;
+	
+	printf("  Ordenação e troca de valores!!\n");
+	printf("- - - - - - - - -- - - - - - - - - - -\n");
+	
+	printf("Informe um valor:\n");
+	scanf("%d", &a);
+	
+	printf("Informe um valor:\n");
+	scanf("%d", &b);
+	
+	printf("Informe um valor:\n");
+	scanf("%d", &c);
+	
+	if(a>b || a>c){
+		if(b<c){
+			troca(&a, &b);
+		}else{
+			troca(&a, &c);
+		}
+	}
+	if(b>c){
+		troca(&b, &c);
+	}
+
+	printf("Os valores ordenados são: %d, %d, %d\n\n", a, b, c);
+	
+	printf("Obrigada por interagir!!");
+	return 0;
+}
